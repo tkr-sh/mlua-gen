b83a38fa4eed734513485ecb320c914503795b5a
tk <49250442+tkr-sh@users.noreply.github.com>
2025-01-18 01:03:02 +0000

    🐛 fix: impl of enums

--- src/enum.rs
+++ src/enum.rs
@@ -1,14 +1,14 @@
-use proc_macro2::Span;
-use quote::quote;
-use quote::ToTokens;
-use syn::Ident;
-use syn::Variant;
+use {
+    proc_macro2::Span,
+    quote::{quote, ToTokens},
+    syn::{Ident, Variant},
+};
 
 pub(crate) fn enum_builder<'l, I: Iterator<Item = &'l Variant>>(
     name: &Ident,
     variants: I,
-    _custom_field: Option<syn::Ident>,
-    _custom_method_or_fn: Option<syn::Ident>,
+    custom_field: Option<syn::Ident>,
+    custom_method_or_fn: Option<syn::Ident>,
 ) -> proc_macro2::TokenStream {
     // Create the fields to access a value.
     let (fields, (fn_constructors, field_constructors)): (Vec<_>, (Vec<_>, Vec<_>)) = variants
@@ -17,7 +17,7 @@ pub(crate) fn enum_builder<'l, I: Iterator<Item = &'l Variant>>(
             let variant_ident = syn::Ident::new(&original_variant_string, Span::call_site());
             let variant_accessor_name = &original_variant_string.to_lowercase();
 
-            let (match_stmt, constructor) = match &variant.fields {
+            let (match_stmt_get, field_set, constructor) = match &variant.fields {
                 // For enum fields that are named
                 //
                 // ```
@@ -28,25 +28,26 @@ pub(crate) fn enum_builder<'l, I: Iterator<Item = &'l Variant>>(
                 // }
                 // ```
                 syn::Fields::Named(field_named) => {
-                    let (set, (fields, field_constructors)): (Vec<_>, (Vec<_>, Vec<_>)) = field_named
-                        .named
-                        .iter()
-                        .map(|named| named.ident.clone().expect("Is named"))
-                        .map(|ident| {
-                            let stringified_ident = ident.to_token_stream().to_string();
-                            (
-                                quote!(
-                                    let _ = table.set(#stringified_ident, #ident.to_owned());
-                                ),
+                    let (set, (fields, field_constructors)): (Vec<_>, (Vec<_>, Vec<_>)) =
+                        field_named
+                            .named
+                            .iter()
+                            .map(|named| named.ident.clone().expect("Is named"))
+                            .map(|ident| {
+                                let stringified_ident = ident.to_token_stream().to_string();
                                 (
-                                    quote!(#ident),
                                     quote!(
-                                        #ident: table.get(#stringified_ident).expect("Missing data"),
+                                        let _ = table.set(#stringified_ident, #ident.to_owned());
+                                    ),
+                                    (
+                                        quote!(#ident),
+                                        quote!(
+                                            #ident: table.get(#stringified_ident)?,
+                                        ),
                                     ),
                                 )
-                            )
-                        })
-                        .unzip();
+                            })
+                            .unzip();
 
                     (
                         quote!(
@@ -58,6 +59,17 @@ pub(crate) fn enum_builder<'l, I: Iterator<Item = &'l Variant>>(
                                 }),
                                 _ => None
                             }
+                        ),
+                        quote!(
+                            reserved_fields.add_field_method_set(
+                                #variant_accessor_name,
+                                |lua, this, table: ::mlua::Table| {
+                                    *this = Self::#variant_ident {
+                                        #(#field_constructors)*
+                                    };
+                                    Ok(())
+                                }
+                            );
                         ),
                         quote!(
                             methods.add_function(
@@ -78,40 +90,51 @@ pub(crate) fn enum_builder<'l, I: Iterator<Item = &'l Variant>>(
                 // ```
                 syn::Fields::Unnamed(field_unnamed) => {
                     // For field access
-                    let (set, fields): (Vec<_>, Vec<_>) = (0..field_unnamed.unnamed.len())
+                    let (set, field_idents): (Vec<_>, Vec<_>) = (0..field_unnamed.unnamed.len())
                         .map(|idx| {
                             // We name fields `v` + idx
                             let ident = syn::Ident::new(&format!("v{idx}"), Span::call_site());
 
-                            (quote!(let _ = table.push(#ident.to_owned());), quote!(#ident))
+                            (
+                                quote!(let _ = table.push(#ident.to_owned());),
+                                quote!(#ident),
+                            )
                         })
                         .unzip();
 
                     // For enum declaration
-                    let (argument, ty) = match field_unnamed.unnamed.len() {
-                        0 => (quote!(), quote! {()}),
+                    let (argument, indexed, ty) = match field_unnamed.unnamed.len() {
+                        0 => (quote!(), quote!(), quote! {()}),
                         // `(my_type)` <=> `my_type` and therefore `.0` won't work
                         1 => {
                             let ty = &field_unnamed.unnamed.first().expect("Length matched").ty;
 
-                            (quote!(param), quote!((#ty)))
+                            (quote!(param), quote!(value.get(0)?), quote!((#ty)))
                         },
                         _ => {
                             let args_ident = (0..field_unnamed.unnamed.len()).map(|idx| {
                                 let index = syn::Index::from(idx);
                                 quote!(param.#index)
                             });
+                            let args_indexed = (1..=field_unnamed.unnamed.len()).map(|idx| {
+                                let index = syn::Index::from(idx);
+                                quote!(value.get(#index)?)
+                            });
 
                             let tys = field_unnamed.unnamed.iter().map(|arg| &arg.ty);
 
-                            (quote!(#(#args_ident),*), quote!((#(#tys),*)))
+                            (
+                                quote!(#(#args_ident),*),
+                                quote!(#(#args_indexed),*),
+                                quote!((#(#tys),*)),
+                            )
                         },
                     };
 
                     (
                         quote!(
                             match this {
-                                Self::#variant_ident ( #(#fields),* ) => Some({
+                                Self::#variant_ident ( #(#field_idents),* ) => Some({
                                     let mut table = lua.create_table().unwrap();
                                     #(#set)*
                                     table
@@ -120,11 +143,24 @@ pub(crate) fn enum_builder<'l, I: Iterator<Item = &'l Variant>>(
                             }
                         ),
                         quote!(
+                            reserved_fields.add_field_method_set(
+                                #variant_accessor_name,
+                                |lua, this, value: ::mlua::Table| {
+                                    println!("{value:#?}");
+                                    // println!("{:#?}", value.get(0).unwrap() as String);
+                                    *this = Self::#variant_ident(
+                                        #indexed
+                                    );
+                                    Ok(())
+                                }
+                            );
+                        ),
+                        quote!(
                             methods.add_function(
                                 #original_variant_string,
                                 |_, param: #ty| Ok(Self::#variant_ident(#argument))
                             )
-                        )
+                        ),
                     )
                 },
                 // For enum fields that don't have a value associated to them
@@ -134,59 +170,69 @@ pub(crate) fn enum_builder<'l, I: Iterator<Item = &'l Variant>>(
                 //     Unit,
                 // }
                 // ```
-                syn::Fields::Unit => (
-                    quote!(
-                        match this { Self::#variant_ident => Some(true), _ => None}
-                    ),
-                    quote!(
-                        fields.add_field_method_get(
-                            #original_variant_string,
-                            |_, _| Ok(Self::#variant_ident),
-                        )
-                    ),
-                ),
+                syn::Fields::Unit => {
+                    (
+                        quote!(
+                            match this { Self::#variant_ident => Some(true), _ => None}
+                        ),
+                        quote!(),
+                        quote!(
+                            reserved_fields.add_field_method_get(
+                                #original_variant_string,
+                                |_, _| Ok(Self::#variant_ident),
+                            )
+                        ),
+                    )
+                },
             };
-
-
 
             (
                 quote!(
-                    fields.add_field_method_get(
+                    reserved_fields.add_field_method_get(
                         #variant_accessor_name,
-                        |lua, this| Ok(#match_stmt)
+                        |lua, this| Ok(#match_stmt_get)
                     );
+                    #field_set
+
+                    // reserved_fields.add_field_method_set(
+                    //     #variant_accessor_name,
+                    //     |lua, this, value| Ok(#match_stmt_get)
+                    // );
                 ),
                 if matches!(variant.fields, syn::Fields::Unit) {
                     (quote!(), constructor)
-
                 } else {
                     (constructor, quote!())
-                }
+                },
             )
         })
         .unzip();
     // .collect::<Vec<TokenStream>>();
 
-    // let method = if let Some(method_or_fn) = custom_method_or_fn {
-    //         quote!(#method_or_fn(method_or_fns))
-    //     } else {
-    //         quote!()
-    //     },
+    let extra_fields = if let Some(field) = custom_field {
+        quote! {#field(reserved_fields)}
+    } else {
+        quote!()
+    };
+    let extra_impls = if let Some(method_or_fn) = custom_method_or_fn {
+        quote!(#method_or_fn(methods))
+    } else {
+        quote!()
+    };
 
     quote! {
         impl ::mlua::UserData for #name {
-            fn add_fields<T: ::mlua::UserDataFields<Self>>(fields: &mut T) {
+            fn add_fields<T: ::mlua::UserDataFields<Self>>(reserved_fields: &mut T) {
                 #(#fields)*
                 #(#field_constructors);*
-                // #(#field_set)*
-                // #field_extra
-                // todo!();
+                ;
+                #extra_fields
             }
 
             fn add_methods<M: ::mlua::UserDataMethods<Self>>(methods: &mut M) {
                 #(#fn_constructors);*
-                // #method_or_fn_extra
-                // todo!();
+                ;
+                #extra_impls
             }
         }
     }
