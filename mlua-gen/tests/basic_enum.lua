f7e680cd486a9e3a3467e7145d66cae2e1694f5d
tk <49250442+tkr-sh@users.noreply.github.com>
2025-01-26 02:06:45 +0100

    test: improve test

--- mlua-gen/tests/basic_enum.lua
+++ mlua-gen/tests/basic_enum.lua
@@ -2,12 +2,17 @@ local pig = Animal.Pig
 local dog = Animal.Dog("doggo", 20)
 local cat = Animal.Cat({ name = "nyan", age = 20 })
 
+-- Accessing basic data 
+---- Pig
 assert(pig.pig)
+---- Dog
 assert(dog.dog[1] == "doggo")
 assert(dog.dog[2] == 20)
+---- Cat
 assert(cat.cat.name == "nyan")
 assert(cat.cat.age == 20)
 
+-- Changing value
 local new_dog = dog.dog
 new_dog[1] = "doggy"
 assert(dog.dog[1] == "doggo")
@@ -16,3 +21,16 @@ dog.dog = new_dog
 
 assert(dog.dog[1] == "doggy")
 assert(dog.dog[2] == 20)
+
+
+-- Nil when accessing wrong value
+assert(pig.dog == nil)
+assert(pig.cat == nil)
+
+assert(dog.pig == nil)
+assert(dog.cat == nil)
+
+assert(cat.pig == nil)
+assert(cat.dog == nil)
+
+-- Note: If you try to do cat.Pig or cat.Dog or cat.Cat, it will error.
